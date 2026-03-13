093e40ef06b31a70acc7785d4e013588c183b74a
miguelroman1 <23308060610314@cetis61.edu.mx>
2026-03-13 17:51:30 -0600

    actu

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import flet as ft
 def main(page: ft.Page):
     page.title = "Login"
     page.padding = 20
-    page.scroll = "adaptive"    
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
     usuario_valido = "admin@gmail.com"
     password_valido = "1234"
@@ -13,11 +14,11 @@ def main(page: ft.Page):
         size=30,
         weight=ft.FontWeight.BOLD,
     )
-    
+
     correo = ft.TextField(
         label="Correo electrónico",
         prefix_icon=ft.Icons.PERSON,
-        width=400,
+        width=350,
     )
 
     contraseña = ft.TextField(
@@ -25,78 +26,112 @@ def main(page: ft.Page):
         prefix_icon=ft.Icons.KEY,
         password=True,
         can_reveal_password=True,
-        width=400
+        width=350
     )
 
     mensaje = ft.Text("", color="red")
 
+    def pantalla_principal():
+
+        page.clean()
+
+        # Barra inferior
+        page.navigation_bar = ft.NavigationBar(
+            destinations=[
+                ft.NavigationBarDestination(icon=ft.Icons.HOME, label="Inicio"),
+                ft.NavigationBarDestination(icon=ft.Icons.EXPLORE, label="Explorar"),
+                ft.NavigationBarDestination(icon=ft.Icons.PERSON, label="Perfil"),
+            ]
+        )
+
+        # Panel superior
+        panel = ft.Container(
+            content=ft.Text(
+                "Panel Principal",
+                size=20,
+                color="white"
+            ),
+            bgcolor=ft.Colors.BLACK87,
+            padding=10,
+            width=page.width
+        )
+
+        bienvenida = ft.Text(
+            "Bienvenido al Sistema",
+            size=30,
+            weight=ft.FontWeight.BOLD
+        )
+
+        subtitulo = ft.Text(
+            "Has iniciado sesión correctamente.",
+            color="grey"
+        )
+
+        page.add(
+            ft.Column(
+                [
+                    panel,
+                    ft.Container(height=120),
+                    bienvenida,
+                    subtitulo
+                ],
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            expand=True
+            )
+        )
+
+        page.update()
+
     def login(e):
+
         if correo.value == usuario_valido and contraseña.value == password_valido:
-            mensaje.value = "Inicio de sesión exitoso"
-            mensaje.color = "green"
             pantalla_principal()
         else:
             mensaje.value = "Correo o contraseña incorrectos"
             mensaje.color = "red"
-
-        page.update()
-        
-    def pantalla_principal():
-        
-        page.clean()
-        
-        page.navigation_bar = ft.NavigationBar(
-        destinations=[
-            ft.NavigationBarDestination(icon=ft.Icons.HOME, label="Inicio"),
-            ft.NavigationBarDestination(icon=ft.Icons.EXPLORE, label="Explorar"),
-            ft.NavigationBarDestination(icon=ft.Icons.PERSON,label="Perfil",
-            ),
-        ]
-    )
-    page.update()
+            page.update()
 
     iniciar_sesion = ft.ElevatedButton(
         "Iniciar sesión",
-        width=250,
+        width=200,
         on_click=login,
         style=ft.ButtonStyle(
             bgcolor=ft.Colors.BLUE_400,
             color=ft.Colors.WHITE,
-            padding=20,
-            shape=ft.RoundedRectangleBorder(radius=13),
+            padding=15,
+            shape=ft.RoundedRectangleBorder(radius=10),
         ),
     )
-    
+
     registro = ft.ElevatedButton(
         "Registrarme",
         width=200,
         style=ft.ButtonStyle(
             bgcolor=ft.Colors.BLUE_800,
             color=ft.Colors.WHITE,
-            padding=20,
-            shape=ft.RoundedRectangleBorder(radius=13),
+            padding=15,
+            shape=ft.RoundedRectangleBorder(radius=10),
         ),
     )
-    
-    link_forgot= ft.Button(
-        "¿Olvidaste tu contraseña?"
-    )
+
+    link_forgot = ft.TextButton("¿Olvidaste tu contraseña?")
 
     page.add(
-        ft.Column([
-            titulo,
-            ft.Container(height=10),
-            correo,
-            ft.Container(height=10),
-            contraseña,
-            ft.Container(height=10),
-            mensaje,
-            ft.Container(height=10),
-            link_forgot,
-            ft.Row([iniciar_sesion, registro], alignment=ft.MainAxisAlignment.CENTER)
-        ],
-        spacing=15,
-        horizontal_alignment=ft.CrossAxisAlignment.CENTER)
+        ft.Column(
+            [
+                titulo,
+                correo,
+                contraseña,
+                mensaje,
+                link_forgot,
+                ft.Row(
+                    [iniciar_sesion, registro],
+                    alignment=ft.MainAxisAlignment.CENTER
+                )
+            ],
+            spacing=15,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER
+        )
     )
 
 ft.run(main)
